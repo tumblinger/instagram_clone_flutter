3de59fa6733ffcc76c2eb548d1b50d30ba74eb05
Valentina <egorova_valentin@mail.ru>
2025-04-10 20:42:17 -0400

    Update a file

--- lib/user_profile/user_profile_model.dart
+++ lib/user_profile/user_profile_model.dart
@@ -1,25 +1,39 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:instagram_clone/user_profile/user_profile_enums.dart';
 
 class UserProfileModel {
   final String uid;
   final String email;
   final String avatar;
+  final String userName;
   final String ? bio;
+  final String ? website;
   final String ? firstName;
-  final String ? gender;
+  final Gender ? gender;
+  final int ? phoneNumber;
+  final DateTime createdAt;
+  final DateTime updatedAt;
 
   UserProfileModel({
     required this.uid,
     required this.email,
     required this.avatar,
+    required this.userName,
     this.bio,
+    this.website,
     this.firstName,
     this.gender,
+    this.phoneNumber,
+    required this.createdAt,
+    required this.updatedAt,
   });
   factory UserProfileModel.fromFirebaseUser(User firebaseUser){
     return UserProfileModel(
         uid: firebaseUser.uid,
         email: firebaseUser.email!,
-        avatar: '');
+        avatar: '',
+        createdAt: DateTime.now(),
+        updatedAt: DateTime.now(),
+        userName: '');
   }
 }
