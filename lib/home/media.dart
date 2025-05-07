5dd3f9f0ec014f5d65021f3683169a69289a8427
tumblinger <77502878+tumblinger@users.noreply.github.com>
2025-05-06 20:25:55 -0400

    Update media.dart

--- lib/home/media.dart
+++ lib/home/media.dart
@@ -8,4 +8,20 @@ class Media {
     required this.value,
     required this.type,
 });
+  factory Media.fromMap(Map<String, dynamic> mediaMap) {
+    return Media(
+      value: mediaMap['value'] as String,
+      type: _stringToMediaType(mediaMap['type']),
+    );
+  }
+  static MediaTypes _stringToMediaType(String stringMediaType) {
+    switch (stringMediaType) {
+      case 'image':
+        return MediaTypes.image;
+      case 'video':
+        return MediaTypes.video;
+      default:
+        throw ArgumentError('Unknown media type - $stringMediaType');
+    }
+  }
 }
