5456f7df25fb83746f0b83be7d3f42d94ca337d4
tumblinger <77502878+tumblinger@users.noreply.github.com>
2025-05-06 20:27:15 -0400

    Update home.screen.dart

--- lib/home/home.screen.dart
+++ lib/home/home.screen.dart
@@ -1,28 +1,51 @@
 import 'package:flutter/material.dart';
-import 'package:instagram_clone/app_routes.dart';
-import 'package:instagram_clone/auth/auth_service.dart';
+import 'package:instagram_clone/home/posts.dart';
+import 'package:instagram_clone/home/posts_service.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final AuthService _authService = AuthService();
+    final PostsService postsService = PostsService();
+
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: Column(
-            children: [
-              const Text("Home Screen!!!",
-                style: TextStyle(fontSize: 24),),
-              ElevatedButton(onPressed: (){
-                _authService.logout();
-                Navigator.pushReplacementNamed(context, AppRoutes.login);
-                print('Log Out');
-              }, child: const Text('Logout'),),
-            ],
-          ),
-        ),
+          child: StreamBuilder<List<Posts>>(
+              stream: postsService.getPosts(),
+              builder: (context, snapshot){
+                //waiting:
+                if(snapshot.connectionState == ConnectionState.waiting){
+                  return const Scaffold(
+                    body: Center(
+                        child: CircularProgressIndicator()));
+                }
+                //error:
+                if(snapshot.hasError){
+                  print('Error: ${snapshot.error}');
+                  return const Center(child: Text('Error'));
+                }
+                // empty- no posts:
+                if(!snapshot.hasData || snapshot.data!.isEmpty){
+                  return const Center(child: Text('No posts yet'));
+                }
+                List<Posts> posts = snapshot.data!;
+
+                return ListView.builder(
+                  itemCount: posts.length,
+                    itemBuilder: (context, index){
+                      Posts post = posts[index];
+
+                      return Column(
+                        children: [
+                          Text(post.userName),
+                          Text(post.caption),
+                          Text(post.likes.toString()),
+                        ],
+                      );
+
+                    });
+              })
       ),
     );
   }
