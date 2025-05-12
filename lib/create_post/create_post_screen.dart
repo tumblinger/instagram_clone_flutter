5cda2935157cb8d19841d6ba97ae4a32b4165f96
tumblinger <77502878+tumblinger@users.noreply.github.com>
2025-05-12 19:20:15 -0400

    Create create_post_screen.dart

---
+++ lib/create_post/create_post_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CreatePostScreen extends StatelessWidget {
+  const CreatePostScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(child: Center(child: Text("Create a post Screen"),)),
+    );
+  }
+}
