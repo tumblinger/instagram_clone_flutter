74422628a2371814f8cf8ccac023c48dcc4c0e8a
tumblinger <77502878+tumblinger@users.noreply.github.com>
2025-05-12 19:20:51 -0400

    Create user_profile_screen.dart

---
+++ lib/user_profile/user_profile_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class UserProfileScreen extends StatelessWidget {
+  const UserProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(child: Center(child: Text("User Profile Screen"),)),
+    );
+  }
+}
