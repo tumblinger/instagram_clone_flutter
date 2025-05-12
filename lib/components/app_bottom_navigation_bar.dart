7d35b68d15fcabeeddb23ca30e7cb1033e09f338
tumblinger <77502878+tumblinger@users.noreply.github.com>
2025-05-11 21:13:21 -0400

    Update app_bottom_navigation_bar.dart

--- lib/components/app_bottom_navigation_bar.dart
+++ lib/components/app_bottom_navigation_bar.dart
@@ -11,16 +11,33 @@ class _AppBottomNavigationBarState extends State<AppBottomNavigationBar> {
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
+        backgroundColor: Colors.white,
         selectedItemColor: Colors.black,
         unselectedItemColor: Colors.black,
         type: BottomNavigationBarType.fixed,
-        backgroundColor: Colors.pinkAccent,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
         items: [
-      BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: ''),
-      BottomNavigationBarItem(icon: Icon(Icons.search_outlined), label: ''),
-      BottomNavigationBarItem(icon: Icon(Icons.add_box_outlined), label: ''),
-      BottomNavigationBarItem(icon: Icon(Icons.video_collection_outlined), label: ''),
-      BottomNavigationBarItem(icon: Icon(Icons.person), label: ''),
+      BottomNavigationBarItem(
+          icon: Icon(Icons.home_outlined),
+          activeIcon: Icon(Icons.home),
+          label: ''),
+      BottomNavigationBarItem(
+          icon: Icon(Icons.search_outlined),
+          activeIcon: Icon(Icons.search),
+          label: ''),
+      BottomNavigationBarItem(
+          icon: Icon(Icons.add_box_outlined),
+          activeIcon: Icon(Icons.add_box),
+          label: ''),
+      BottomNavigationBarItem(
+          icon: Icon(Icons.video_collection_outlined),
+          activeIcon: Icon(Icons.video_collection),
+          label: ''),
+      BottomNavigationBarItem(
+          icon: Icon(Icons.person_2_outlined),
+          activeIcon: Icon(Icons.person_2),
+          label: ''),
     ]);
   }
 }
