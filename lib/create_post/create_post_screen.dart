8ddf24c2cea931464fd5bbeddbe6a866d0f91597
tumblinger <77502878+tumblinger@users.noreply.github.com>
2025-05-12 21:13:47 -0400

    Update create_post_screen.dart

--- lib/create_post/create_post_screen.dart
+++ lib/create_post/create_post_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../components/app_bottom_navigation_bar.dart';
+
 class CreatePostScreen extends StatelessWidget {
   const CreatePostScreen({super.key});
 
@@ -7,6 +9,7 @@ class CreatePostScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: SafeArea(child: Center(child: Text("Create a post Screen"),)),
+        bottomNavigationBar:  AppBottomNavigationBar(currentIndex: 2)
     );
   }
 }
