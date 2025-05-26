c44b68ba326003a7b43e3cc9ac36188200db7c1b
tumblinger <77502878+tumblinger@users.noreply.github.com>
2025-05-25 20:00:36 -0400

    Update home_post_details_card.dart

--- lib/home/home_components/home_post_details_card.dart
+++ lib/home/home_components/home_post_details_card.dart
@@ -7,15 +7,42 @@ class HomePostDetailsCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(children: [
-          Row(children: [
-            const Icon(Icons.favorite_outline),
-                Text('${post.likes}'),
-          ],)
-        ],)
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+      child: Column(
+        children: [
+         Row(
+           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+           children: [
+             Row(
+               children: [
+                 HomePostDetailsStatistic(icon: Icons.favorite_outline, statValue: post.likes),
+                 const SizedBox(width: 12.0),
+                 HomePostDetailsStatistic(icon: Icons.chat_bubble_outline, statValue: post.comments),
+                 const SizedBox(width: 12.0),
+                 HomePostDetailsStatistic(icon: Icons.send_outlined, statValue: post.shares),
+               ],
+             ),
+             Icon(Icons.bookmark_border_outlined)
+           ],
+         )
+        ],
+      ),
     );
   }
 }
+
+class HomePostDetailsStatistic extends StatelessWidget {
+  final IconData icon;
+  final int statValue;
+  const HomePostDetailsStatistic({super.key, required this.icon, required this.statValue});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [
+      Icon(icon),
+      Text('$statValue'),
+    ],);
+  }
+}
+
