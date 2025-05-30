c45f2435ce6526fc2e6760aa213173a1f1ca55d4
tumblinger <77502878+tumblinger@users.noreply.github.com>
2025-05-29 20:27:35 -0400

    Create posts.dart

---
+++ lib/create_post/posts.dart
@@ -0,0 +1,48 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:instagram_clone/home/media.dart';
+
+class Posts {
+  final String userName;
+  final String avatar;
+  final List <Media> media;
+  final String caption;
+  final int likes;
+  final int shares;
+  final int comments;
+  final DateTime createdAt;
+  final DateTime updatedAt;
+
+  Posts ({
+    required this.userName,
+    required this.avatar,
+    required this.media,
+    required this.caption,
+    required this.likes,
+    required this.shares,
+    required this.comments,
+    required this.createdAt,
+    required this.updatedAt,
+});
+
+  factory Posts.fromFirestore(DocumentSnapshot firestorePostDoc, DocumentSnapshot firestoreUserProfileDoc){
+
+    Map<String, dynamic> firestorePostData = firestorePostDoc.data() as Map<String, dynamic>;
+    Map<String, dynamic> firestoreUserProfileData = firestoreUserProfileDoc.data() as Map<String, dynamic>;
+
+    List<Media> media = (firestorePostData['media'] as List)
+        .map((media) => Media.fromMap(media as Map<String, dynamic>))
+        .toList();
+
+    return Posts(
+        userName: firestoreUserProfileData['userName'],
+        avatar: firestoreUserProfileData['avatar'],
+        media: media,
+        caption: firestorePostData['caption'],
+        likes: firestorePostData['likes'],
+        shares: firestorePostData['shares'],
+        comments: firestorePostData['comments'],
+        createdAt: (firestorePostData['createdAt'] as Timestamp).toDate(),
+        updatedAt: (firestorePostData['updatedAt'] as Timestamp).toDate(),
+    );
+  }
+}
