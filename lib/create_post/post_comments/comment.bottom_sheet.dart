ba69aca3a77d7c12d2752092add7832102f0bfc0
tumblinger <77502878+tumblinger@users.noreply.github.com>
2025-07-12 15:01:48 -0400

    Update comment.bottom_sheet.dart

--- lib/create_post/post_comments/comment.bottom_sheet.dart
+++ lib/create_post/post_comments/comment.bottom_sheet.dart
@@ -1,1 +1,13 @@
+import 'package:flutter/material.dart';
+class CommentBottomSheet extends StatelessWidget {
+  const CommentBottomSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height*0.75,
+      color: Colors.redAccent,
+    );
+  }
+}
 
