06ff74536bb13289b73ab32604cc5c7929fd5c9b
tumblinger <77502878+tumblinger@users.noreply.github.com>
2025-07-13 15:38:58 -0400

    Update comment.bottom_sheet.dart

--- lib/create_post/post_comments/comment.bottom_sheet.dart
+++ lib/create_post/post_comments/comment.bottom_sheet.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
-class CommentBottomSheet extends StatelessWidget {
+import 'package:instagram_clone/user_profile/user_profile_model.dart';
+import 'package:provider/provider.dart';
+
+import '../../user_profile/user_profile_provider.dart';
+
+class CommentBottomSheet extends StatefulWidget {
   const CommentBottomSheet({super.key});
 
   @override
+  State<CommentBottomSheet> createState() => _CommentBottomSheetState();
+}
+
+class _CommentBottomSheetState extends State<CommentBottomSheet> {
+  @override
   Widget build(BuildContext context) {
+    final userProfile = context.watch<MyAuthProvider>().userProfile;
+    print('!!!!!!!!!!!!!!!!!!!!!!!!!!!! userProfile: $userProfile');
     return Container(
       height: MediaQuery.of(context).size.height*0.75,
       width: MediaQuery.of(context).size.width,
@@ -12,7 +24,20 @@ class CommentBottomSheet extends StatelessWidget {
           SizedBox(
             height: 8),
           Text('Comments', style: TextStyle(fontWeight: FontWeight.bold)),
-          SizedBox(height: 8)
+          SizedBox(height: 8),
+
+          Row(children: [
+
+            if(userProfile?.avatar != null)
+              SizedBox(
+                width: 28.0,
+                child: CircleAvatar(
+                    backgroundImage: NetworkImage(userProfile!.avatar)),
+              ),
+
+
+          ],)
+
         ],
       ),
     );
