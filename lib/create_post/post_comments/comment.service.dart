12e0d2061a8d56b50eb1928c27065d5ab5d86ab3
tumblinger <77502878+tumblinger@users.noreply.github.com>
2025-07-17 19:22:08 -0400

    Update comment.service.dart

--- lib/create_post/post_comments/comment.service.dart
+++ lib/create_post/post_comments/comment.service.dart
@@ -1,1 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+class CommentService {
+  final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
+  Future<void> addComment (String postId, String userId, String text) async{
+    try{
+      final commentBatch = _firebaseFirestore.batch();
+      final commentRef = _firebaseFirestore.collection('comments').doc();
+      commentBatch.set(commentRef, {
+        'postId', postId,
+        'userId', _firebaseFirestore.collection('user-profiles').doc(userId),
+        'text', text,
+        'createdAt', Timestamp.now(),
+        'updatedAt', Timestamp.now(),
+      });
+    } catch(error){
+      
+    }
+  }
+}
