19c49a76ab989c029f1e56a2cc1cb83669a090f7
tumblinger <77502878+tumblinger@users.noreply.github.com>
2025-07-19 19:01:20 -0400

    Update comment.service.dart

--- lib/create_post/post_comments/comment.service.dart
+++ lib/create_post/post_comments/comment.service.dart
@@ -1,9 +1,31 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:instagram_clone/create_post/posts_service.dart';
 
+import 'comment.dart';
+
 class CommentService {
   final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
   final PostsService _postsService = PostsService();
+
+  Stream<List<Comment>> getPostComments(String postId){
+    return
+      _firebaseFirestore
+          .collection('comments')
+          .orderBy('createdAt', descending: true)
+          .snapshots().asyncMap((snapshot) async {
+        List<Comment> comments = [];
+        for(
+        QueryDocumentSnapshot<Map<String, dynamic>> firestorePostDoc in snapshot.docs)
+        {
+          DocumentReference userProfileRef = firestorePostDoc['createdby'];
+          DocumentSnapshot firestoreUserProfileDoc = await userProfileRef.get();
+
+          comments.add(Comment.fromFirestore(firestorePostDoc, firestoreUserProfileDoc));
+
+        }
+        return comments;
+      });
+  }
 
   Future<void> addComment (String postId, String userId, String text) async{
     try{
