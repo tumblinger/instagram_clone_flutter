d5e413d7e94d420c4e4f677153b4bb5129e2ee9e
tumblinger <77502878+tumblinger@users.noreply.github.com>
2025-07-22 20:27:49 -0400

    Update and rename search_screen.dart to gallery_screen.dart

--- lib/search/gallery_screen.dart
+++ lib/search/gallery_screen.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
-
 import '../components/app_bottom_navigation_bar.dart';
 
-class SearchScreen extends StatelessWidget {
-  const SearchScreen({super.key});
+class GalleryScreen extends StatelessWidget {
+  const GalleryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: SafeArea(child: Center(
-        child: Text("Search Screen"),)),
+        child: Text("Gallery Screen"),)),
         bottomNavigationBar:  AppBottomNavigationBar(currentIndex: 1)
     );
   }
