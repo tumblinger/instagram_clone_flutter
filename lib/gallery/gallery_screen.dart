cc00b8d8a74353682a331c61a7fb233020a44f5e
tumblinger <77502878+tumblinger@users.noreply.github.com>
2025-07-22 20:54:48 -0400

    Update gallery_screen.dart

--- lib/gallery/gallery_screen.dart
+++ lib/gallery/gallery_screen.dart
@@ -1,15 +1,43 @@
 import 'package:flutter/material.dart';
 import '../components/app_bottom_navigation_bar.dart';
 
-class GalleryScreen extends StatelessWidget {
+class GalleryScreen extends StatefulWidget {
+
   const GalleryScreen({super.key});
 
   @override
+  State<GalleryScreen> createState() => _GalleryScreenState();
+}
+
+class _GalleryScreenState extends State<GalleryScreen> {
+  final TextEditingController _searchController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(child: Center(
-        child: Text("Gallery Screen"),)),
-        bottomNavigationBar:  AppBottomNavigationBar(currentIndex: 1)
+    return Scaffold(
+      body:
+          SafeArea(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 8),
+                    child: TextField(
+                      controller: _searchController,
+                      keyboardType: TextInputType.text,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(100),
+                            borderSide: BorderSide.none
+                        ),
+                        hintText: "Search by username...",
+                        filled: true,
+                        fillColor: Colors.black12
+                      ),
+                    ),
+                  )
+                ],
+              )),
+        bottomNavigationBar:  const AppBottomNavigationBar(currentIndex: 1)
     );
   }
 }
