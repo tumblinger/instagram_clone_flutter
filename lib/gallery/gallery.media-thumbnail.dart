fe52fc7f888577e84d5abe8754013925cab52e26
tumblinger <77502878+tumblinger@users.noreply.github.com>
2025-07-24 20:30:25 -0400

    Update gallery.media-thumbnail.dart

--- lib/gallery/gallery.media-thumbnail.dart
+++ lib/gallery/gallery.media-thumbnail.dart
@@ -1,1 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/home/media.dart';
+
+class GalleryMediaThumbnail extends StatelessWidget {
+  final Media media;
+  const GalleryMediaThumbnail({super.key, required this.media});
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final mediaSize = screenWidth/3;
+    
+    return SizedBox(
+      width: mediaSize,
+      height: mediaSize,
+      child: media.type == MediaTypes.image 
+          ? Image.network(media.value, fit: BoxFit.cover) 
+          : null,
+    );
+  }
+}
 
