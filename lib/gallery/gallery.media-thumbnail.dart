34741ac254bb8b8ef18740c7171706c243869407
tumblinger <77502878+tumblinger@users.noreply.github.com>
2025-07-24 21:23:19 -0400

    Update gallery.media-thumbnail.dart

--- lib/gallery/gallery.media-thumbnail.dart
+++ lib/gallery/gallery.media-thumbnail.dart
@@ -1,22 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/home/media.dart';
+import 'package:video_player/video_player.dart';
 
-class GalleryMediaThumbnail extends StatelessWidget {
+class GalleryMediaThumbnail extends StatefulWidget {
   final Media media;
   const GalleryMediaThumbnail({super.key, required this.media});
+
+  @override
+  State<GalleryMediaThumbnail> createState() => _GalleryMediaThumbnailState();
+}
+
+class _GalleryMediaThumbnailState extends State<GalleryMediaThumbnail> {
+  VideoPlayerController? _videoController;
+  @override
+  void initState() {
+    if(widget.media.type == MediaTypes.video){
+      _initializeVideo();
+    };
+    super.initState();
+  }
+  Future<void> _initializeVideo() async {
+    _videoController = VideoPlayerController.networkUrl(Uri.parse(widget.media.value));
+    await _videoController!.initialize();
+
+    if(mounted) {
+      setState(() {
+        _videoController!.play();
+        _videoController!.setVolume(0);
+        _videoController!.setLooping(true);
+      });
+    }
+  }
+  @override
+  void dispose() {
+    _videoController?.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
     final mediaSize = screenWidth/3;
-    
+
     return SizedBox(
       width: mediaSize,
       height: mediaSize,
-      child: media.type == MediaTypes.image 
-          ? Image.network(media.value, fit: BoxFit.cover) 
-          : null,
+      child: widget.media.type == MediaTypes.image
+          ? Image.network(widget.media.value, fit: BoxFit.cover)
+          : VideoPlayer(_videoController!),
     );
   }
 }
-
