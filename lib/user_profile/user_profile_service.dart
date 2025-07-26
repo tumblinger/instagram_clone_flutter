2c230d1497e3e1fe71cdaab988e3177c0954111c
tumblinger <77502878+tumblinger@users.noreply.github.com>
2025-07-25 20:17:00 -0400

    Update user_profile_service.dart

--- lib/user_profile/user_profile_service.dart
+++ lib/user_profile/user_profile_service.dart
@@ -1,16 +1,20 @@
 import 'dart:convert';
 import 'dart:math';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:crypto/crypto.dart';
+import 'package:instagram_clone/user_profile/user_profile_model.dart';
 
 class UserProfileService {
+  final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
+
   // 0. Create a function
   String userIDGenerator(){
     // 1. Generate cryptographically strong random number
-    Random _random = Random.secure();
+    Random random = Random.secure();
 
     // 2. Create a string of random characters
     String randomString = base64Url.encode(
-        List<int>.generate(16, (_) =>_random.nextInt(256)));
+        List<int>.generate(16, (_) => random.nextInt(256)));
 
     // 3. Create a hash of the randomString using SHA-256
     List<int> bytes = utf8.encode(randomString);
@@ -19,4 +23,48 @@ class UserProfileService {
     return 'user_$shortHash';
   }
 
+  // Function to get User's profile:
+  Future<UserProfileModel?> getUserProfile(String uid) async {
+    final firestoreUserProfileDoc = await _firebaseFirestore.collection('user-profiles').doc(uid).get();
+    if(firestoreUserProfileDoc.exists) {
+      return UserProfileModel.fromFirestore(firestoreUserProfileDoc);
+    }
+    return null;
+  }
+
+  Future<bool> createUserProfile(UserProfileModel userProfileData) async{
+    print('USER: $userProfileData');
+    print(userProfileData.toMap());
+    try{
+      await _firebaseFirestore
+          .collection('user-profiles')
+          .doc(userProfileData.uid)
+          .set(userProfileData.toMap());
+      return true;
+    }
+    catch(e){
+      print(e);
+      return false;
+    }
+  }
+  
+  Future<List<UserProfileModel>> getUsersByUserNameSearch(String searchText) async {
+    if(searchText.isEmpty) return [];
+    
+    //make it case-insensitive:
+    final lowerCaseSearchText = searchText.toLowerCase();
+    
+    try{
+      final querySnapshot = await _firebaseFirestore
+          .collection('user-profiles')
+          .orderBy('userName')
+          .where('userName', isGreaterThanOrEqualTo: lowerCaseSearchText)
+          .where('userName', isLessThan: '${lowerCaseSearchText}z')
+          .get();
+    }
+    catch (error){
+      print(error);
+    }
+  } 
 }
+
