078ecfc591b59116e587755054f15ad60dbeba7a
tumblinger <77502878+tumblinger@users.noreply.github.com>
2025-07-27 17:03:48 -0400

    Update user_page_screen.dart

--- lib/user_profile/user_page_screen.dart
+++ lib/user_profile/user_page_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/components/app_bottom_navigation_bar.dart';
 import 'package:instagram_clone/components/app_follow_button.dart';
+import 'package:instagram_clone/user_profile/user_profile_model.dart';
+import 'package:instagram_clone/user_profile/user_profile_service.dart';
 
-class UserPageScreen extends StatelessWidget {
+class UserPageScreen extends StatefulWidget {
   final int currentScreenIndex;
   final String userId;
   final String userName;
@@ -14,6 +16,36 @@ class UserPageScreen extends StatelessWidget {
     required this.userName});
 
   @override
+  State<UserPageScreen> createState() => _UserPageScreenState();
+}
+
+class _UserPageScreenState extends State<UserPageScreen> {
+  final UserProfileService userProfileService = UserProfileService();
+  bool _isLoadingUserProfile = true;
+  UserProfileModel? _userProfile;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  Future<void> _getUserProfile() async{
+    try{
+      UserProfileModel? userProfile = await userProfileService.getUserProfile(widget.userId);
+      if(userProfile != null){
+        setState(() {
+          _isLoadingUserProfile = false;
+          _userProfile = userProfile;
+        });
+      }
+    }
+    catch(error){
+      rethrow;
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -21,7 +53,7 @@ class UserPageScreen extends StatelessWidget {
           onTap: ()=> Navigator.pop(context),
           child: Icon(Icons.arrow_back),
         ),
-        title: Text(userName, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+        title: Text(widget.userName, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
         actions: [
           AppFollowButton(color: Colors.black)
         ],
@@ -30,7 +62,7 @@ class UserPageScreen extends StatelessWidget {
           child: Center(
               child: Text('User Page Screen')
           )),
-      bottomNavigationBar: AppBottomNavigationBar(currentIndex: currentScreenIndex),
+      bottomNavigationBar: AppBottomNavigationBar(currentIndex: widget.currentScreenIndex),
     );
   }
 }
