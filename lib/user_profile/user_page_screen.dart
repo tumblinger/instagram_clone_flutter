1b31239b6fa4a2abefea6bc00e98d1b15cbb77d0
tumblinger <77502878+tumblinger@users.noreply.github.com>
2025-07-27 14:55:39 -0400

    Update user_page_screen.dart

--- lib/user_profile/user_page_screen.dart
+++ lib/user_profile/user_page_screen.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/components/app_bottom_navigation_bar.dart';
+import 'package:instagram_clone/user_profile/user_profile_model.dart';
 
 class UserPageScreen extends StatelessWidget {
   final int currentScreenIndex;
-  const UserPageScreen({super.key, required this.currentScreenIndex});
+  final UserProfileModel userProfile;
+  
+  const UserPageScreen({
+    super.key, 
+    required this.currentScreenIndex, 
+    required this.userProfile});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: InkWell(
+          onTap: ()=> Navigator.pop(context),
+          child: Icon(Icons.arrow_back),
+        ),
+        title: Text(userProfile.userName, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+        actions: [
+          OutlinedButton(
+              onPressed: (){}, 
+              child: Text('hi'))
+        ],
+      ),
       body: SafeArea(
           child: Center(
               child: Text('User Page Screen')
@@ -16,3 +34,4 @@ class UserPageScreen extends StatelessWidget {
     );
   }
 }
+
