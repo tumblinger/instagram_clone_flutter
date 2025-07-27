1a59841b0d58619e300e9e701f4c22f23ab9f62f
tumblinger <77502878+tumblinger@users.noreply.github.com>
2025-07-27 16:19:04 -0400

    Update user_page_screen.dart

--- lib/user_profile/user_page_screen.dart
+++ lib/user_profile/user_page_screen.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/components/app_bottom_navigation_bar.dart';
+import 'package:instagram_clone/components/app_follow_button.dart';
 import 'package:instagram_clone/user_profile/user_profile_model.dart';
 
 class UserPageScreen extends StatelessWidget {
   final int currentScreenIndex;
-  final UserProfileModel userProfile;
-  
+  final String userId;
+  final String userName;
+
   const UserPageScreen({
     super.key, 
     required this.currentScreenIndex, 
-    required this.userProfile});
+    required this.userId,
+    required this.userName});
 
   @override
   Widget build(BuildContext context) {
@@ -19,11 +22,9 @@ class UserPageScreen extends StatelessWidget {
           onTap: ()=> Navigator.pop(context),
           child: Icon(Icons.arrow_back),
         ),
-        title: Text(userProfile.userName, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+        title: Text(userName, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
         actions: [
-          OutlinedButton(
-              onPressed: (){}, 
-              child: Text('hi'))
+          AppFollowButton()
         ],
       ),
       body: SafeArea(
@@ -34,4 +35,3 @@ class UserPageScreen extends StatelessWidget {
     );
   }
 }
-
