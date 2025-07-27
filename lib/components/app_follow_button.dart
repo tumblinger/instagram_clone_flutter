05beb6187c1dc97c3b8f6923afd1a0dd6bc2563e
tumblinger <77502878+tumblinger@users.noreply.github.com>
2025-07-27 16:40:48 -0400

    Update app_follow_button.dart

--- lib/components/app_follow_button.dart
+++ lib/components/app_follow_button.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
 
 class AppFollowButton extends StatelessWidget {
-  const AppFollowButton({super.key});
+  final Color color;
+  const AppFollowButton({super.key, this.color = Colors.white});
 
   @override
   Widget build(BuildContext context) {
     return OutlinedButton(
         onPressed: (){},
         style: OutlinedButton.styleFrom(
-          side: BorderSide(color: Colors.white, width: 1),
+          side: BorderSide(color: color, width: 1),
           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
           padding: EdgeInsets.symmetric(horizontal: 8, vertical: 2),
           minimumSize: Size.zero
-        ), 
-        child: Text('Follow', 
-          style: TextStyle(color: Colors.white, fontSize: 10)
-        ));
+        ),
+        child: Text('Follow',
+          style: TextStyle(color: color, fontSize: 10)
+        )
+    );
   }
 }
