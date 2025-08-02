19c17306a2aed2758fd1e7585436fcb85b590e55
tumblinger <77502878+tumblinger@users.noreply.github.com>
2025-08-02 16:39:16 -0400

    Update user_page_screen.dart

--- lib/user_profile/user_page_screen.dart
+++ lib/user_profile/user_page_screen.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/components/app_bottom_navigation_bar.dart';
 import 'package:instagram_clone/components/app_follow_button.dart';
+import 'package:instagram_clone/create_post/posts_service.dart';
 import 'package:instagram_clone/user_profile/user_profile_model.dart';
 import 'package:instagram_clone/user_profile/user_profile_service.dart';
+import '../create_post/posts.dart';
+import '../components/gallery.media-thumbnail.dart';
+import '../home/media.dart';
 
 class UserPageScreen extends StatefulWidget {
   final int currentScreenIndex;
@@ -21,8 +25,9 @@ class UserPageScreen extends StatefulWidget {
 
 class _UserPageScreenState extends State<UserPageScreen> {
   final UserProfileService userProfileService = UserProfileService();
+  final PostsService postsService = PostsService();
   bool _isLoadingUserProfile = true; 
-  UserProfileModel? _userProfile;
+  UserProfileModel? _userProfile; 
 
   @override
   void initState() { 
@@ -30,7 +35,7 @@ class _UserPageScreenState extends State<UserPageScreen> {
     super.initState();
   }
 
-  Future<void> _getUserProfile() async{
+  Future<void> _getUserProfile() async{ 
     try{
       UserProfileModel? userProfile = await userProfileService.getUserProfile(widget.userId);
       if(userProfile != null){
@@ -65,34 +70,74 @@ class _UserPageScreenState extends State<UserPageScreen> {
           children: [
             if (_isLoadingUserProfile)
               CircularProgressIndicator(strokeWidth: 2),
-            if(!_isLoadingUserProfile && _userProfile != null)
 
-          Column(
-             children: [
-               if(_userProfile != null)
-               UserPageHeader(userProfileModel: _userProfile!),
-               Container(
-                 decoration: BoxDecoration(
-                   border: Border(
-                     top: BorderSide(color: Colors.black12),
-                     bottom: BorderSide(color: Colors.black12)
-                   )
-                 ),
-                 child: Padding(
-                   padding: const EdgeInsets.symmetric(vertical: 8),
-                   child: Row(
-                     mainAxisAlignment: MainAxisAlignment.center,
-                     children: [
-                       Icon(Icons.grid_on_outlined),
-                       SizedBox(width: 100),
-                       Icon(Icons.video_collection_outlined),
-                       SizedBox(width: 100),
-                       Icon(Icons.image_outlined)
-                     ],
+            if(!_isLoadingUserProfile && _userProfile != null)
+          Expanded(
+            child: Column(
+               children: [
+                 // if(_userProfile != null)
+                 UserPageHeader(userProfileModel: _userProfile!),
+                 Container(
+                   decoration: BoxDecoration(
+                     border: Border(
+                       top: BorderSide(color: Colors.black12),
+                       bottom: BorderSide(color: Colors.black12)
+                     )
+                   ),
+                   child: Padding(
+                     padding: const EdgeInsets.symmetric(vertical: 8),
+                     child: Row(
+                       mainAxisAlignment: MainAxisAlignment.center,
+                       children: [
+                         Icon(Icons.grid_on_outlined),
+                         SizedBox(width: 100),
+                         Icon(Icons.video_collection_outlined),
+                         SizedBox(width: 100),
+                         Icon(Icons.image_outlined)
+                       ],
+                     ),
                    ),
                  ),
-               )
-             ],
+
+                 Expanded(
+                   child: StreamBuilder<List<Posts>>(
+                     stream: postsService.getPostsByUserId(_userProfile!.uid),
+                     builder: (context, snapshot) {
+                       if(snapshot.connectionState == ConnectionState.waiting){
+                         return Center(child: CircularProgressIndicator());
+                       }
+                       if(!snapshot.hasData || snapshot.data!.isEmpty){
+                         return Center(
+                           child: Text('No posts found'),
+                         );
+                       }
+                       final posts = snapshot.data!;
+                       print('Posts count: ${posts.length}');
+
+                       final allMedia = posts.expand((post) => post.media).toList();
+                       print("Total media count: ${allMedia.length}");
+                       if(allMedia.isEmpty){
+                         return Center(
+                           child: Text('No media found')
+                         );
+                       }
+
+                       return GridView.builder(
+                           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                               crossAxisCount: 3,
+                               mainAxisSpacing: 2,
+                               crossAxisSpacing: 2
+                           ),
+                           itemCount: allMedia.length,
+                           itemBuilder: (context, index){
+                             Media media = allMedia[index];
+                             return GalleryMediaThumbnail(media: media,);
+                           });
+                     },
+                   ),
+                 ),
+               ],
+            ),
           )
           ])
       ),
@@ -139,8 +184,8 @@ class UserPageHeader extends StatelessWidget {
             children: [
               Text(userProfileModel.userName, style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold)),
 
-              if(userProfileModel.bio != null)
-                Text(userProfileModel.bio ?? '', style: TextStyle(fontSize: 12, color: Colors.black54)),
+              if(userProfileModel.bio != null && userProfileModel.bio!.isNotEmpty)
+                Text(userProfileModel.bio!, style: TextStyle(fontSize: 12, color: Colors.black54)),
             ],
           )
         ],
@@ -148,7 +193,6 @@ class UserPageHeader extends StatelessWidget {
     );
   }
 }
-
 
 class UserStatistics extends StatelessWidget {
   final int value;
