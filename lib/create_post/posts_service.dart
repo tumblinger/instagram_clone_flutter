5a9c0fb7695fe4009c264c150fa46acd8b6e64f0
tumblinger <77502878+tumblinger@users.noreply.github.com>
2025-08-02 13:37:21 -0400

    Update posts_service.dart

--- lib/create_post/posts_service.dart
+++ lib/create_post/posts_service.dart
@@ -23,16 +23,52 @@ class PostsService {
           return posts;
     });
   }
-  
+
+  Stream<List<Posts>> getPostsByUserId(String userId){
+    return
+      _firebaseFirestore
+          .collection('posts')
+          .orderBy('createdAt', descending: true)
+          .snapshots().asyncMap((snapshot) async {
+        List<Posts> posts = [];
+        for(
+        QueryDocumentSnapshot<Map<String, dynamic>> firestorePostDoc in snapshot.docs)
+        {
+          DocumentReference userProfileRef = firestorePostDoc['createdby'];
+          DocumentSnapshot firestoreUserProfileDoc = await userProfileRef.get();
+
+          posts.add(Posts.fromFirestore(firestorePostDoc, firestoreUserProfileDoc));
+
+        }
+        return posts;
+      });
+  }
+
   Future <void> updatePost(String postId, Map<String, dynamic> updateData) async{
     try{
       final dataToUpdate = Map<String, dynamic>.from(updateData);
       dataToUpdate['updatedAt'] = Timestamp.now();
       await _firebaseFirestore.collection('posts').doc(postId).update(dataToUpdate);
-    } 
+    }
     catch(error){
       print('Error updating post: $error');
       return;
     }
+  }
+
+  Future<void>incrementLikes(String postId) async{
+    await updatePost(postId, {
+      'likes': FieldValue.increment(1)
+    });
+  }
+  Future<void>incrementComments(String postId) async{
+    await updatePost(postId, {
+      'comments': FieldValue.increment(1)
+    });
+  }
+  Future<void>incrementShares(String postId) async{
+    await updatePost(postId, {
+      'shares': FieldValue.increment(1)
+    });
   }
 }
