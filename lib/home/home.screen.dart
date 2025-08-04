6309ad2985fc78cce31d4d0f55a23ae9a6c27513
tumblinger <77502878+tumblinger@users.noreply.github.com>
2025-08-04 15:18:45 -0400

    Update home.screen.dart

--- lib/home/home.screen.dart
+++ lib/home/home.screen.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:instagram_clone/auth/auth_components/post_list_view.dart';
 import 'package:instagram_clone/create_post/posts.dart';
 import 'package:instagram_clone/create_post/posts_service.dart';
-import 'package:instagram_clone/home/home_components/home_post_details_card.dart';
-
 import '../app_constants.dart';
 import '../components/app_bottom_navigation_bar.dart';
-import '../components/app_follow_button.dart';
-import '../user_profile/user_page_screen.dart';
-import 'home_components/home_media_slider.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -56,68 +52,11 @@ class HomeScreen extends StatelessWidget {
                 }
                 List<Posts> posts = snapshot.data!;
 
-                return ListView.builder(
-                  itemCount: posts.length,
-                  itemBuilder: (context, index){
-                      Posts post = posts[index];
-
-                      return Card(
-                        elevation: 0.0,
-                        color: Colors.transparent,
-                        child: Column(
-                          children: [
-                            Stack(
-                              children: [
-                                HomeMediaSlider(mediaList: post.media),
-                                Container(
-                                  color: Colors.black12,
-                                  child: Padding(
-                                  padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 12.0),
-                                  child: Row(
-                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                    children: [
-                                    InkWell(
-                                      onTap: () => Navigator.push(
-                                          context, MaterialPageRoute(
-                                          builder: (context) => UserPageScreen(
-                                            currentScreenIndex: currentScreenIndex,
-                                            userId: post.userId, 
-                                            userName: post.userName)
-                                      )),
-                                      child: Row(
-                                        children: [
-                                        SizedBox(
-                                          width: 28.0,
-                                          child: CircleAvatar(
-                                              backgroundImage: NetworkImage(post.avatar)),
-                                        ),
-
-                                        SizedBox(width: 12.0),
-
-                                        Column(
-                                          crossAxisAlignment: CrossAxisAlignment.start,
-                                          children: [
-                                          Text(post.userName, style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.bold, color: Colors.white)),
-                                          const Text('Suggested for you', style: TextStyle(fontSize: 10.0, color: Colors.white))
-                                        ],)
-                                      ],),
-                                    ),
-                                    Row(children: [
-                                      AppFollowButton(),
-                                      SizedBox(width: 12.0),
-                                      Icon(Icons.more_horiz, color: Colors.white)
-                                    ],)
-                                  ],),
-                                  ),
-                                ),
-                              ]),
-
-                            HomePostDetailsCard(post: post, postService: postsService)
-                          ],
-                        ),
-                      );
-
-                    });
+                return PostListView(
+                    posts: posts,
+                    currentScreenIndex: currentScreenIndex,
+                    postsService: postsService
+                );
               })
       ),
         bottomNavigationBar:  AppBottomNavigationBar(currentIndex: currentScreenIndex)
