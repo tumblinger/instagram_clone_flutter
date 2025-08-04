21948adad9b992bab4d277388964d8a44a3e86c7
tumblinger <77502878+tumblinger@users.noreply.github.com>
2025-08-04 15:22:17 -0400

    Create user_posts_screen.dart

---
+++ lib/user_profile/user_posts_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/create_post/posts_service.dart';
+
+class UserPostsScreen extends StatelessWidget {
+  final String userId;
+  UserPostsScreen({super.key, required this.userId});
+  final PostsService postsService = PostsService();
+
+  @override
+  Widget build(BuildContext context) {
+    return
+  }
+}
