1f067f087c9a64f66e25375c20e12b0a7398f7ce
tumblinger <77502878+tumblinger@users.noreply.github.com>
2025-08-04 15:59:48 -0400

    Update gallery.media-thumbnail.dart

--- lib/gallery/gallery.media-thumbnail.dart
+++ lib/gallery/gallery.media-thumbnail.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/home/media.dart';
+import 'package:instagram_clone/user_profile/user_posts_screen.dart';
 import 'package:video_player/video_player.dart';
+import '../user_profile/user_profile_model.dart';
 
 class GalleryMediaThumbnail extends StatefulWidget {
-  final Media media;
-  const GalleryMediaThumbnail({super.key, required this.media});
+  final UserPostMedia userPostMedia;
+  final int currentScreenIndex;
+
+  const GalleryMediaThumbnail({
+    super.key,
+    required this.currentScreenIndex,
+    required this.userPostMedia
+  });
 
   @override
   State<GalleryMediaThumbnail> createState() => _GalleryMediaThumbnailState();
@@ -14,13 +22,13 @@ class _GalleryMediaThumbnailState extends State<GalleryMediaThumbnail> {
   VideoPlayerController? _videoController;
   @override
   void initState() {
-    if(widget.media.type == MediaTypes.video){
+    if(widget.userPostMedia.media.type == MediaTypes.video){
       _initializeVideo();
     };
     super.initState();
   }
   Future<void> _initializeVideo() async {
-    _videoController = VideoPlayerController.networkUrl(Uri.parse(widget.media.value));
+    _videoController = VideoPlayerController.networkUrl(Uri.parse(widget.userPostMedia.media.value));
     await _videoController!.initialize();
 
     if(mounted) {
@@ -43,23 +51,32 @@ class _GalleryMediaThumbnailState extends State<GalleryMediaThumbnail> {
     final screenWidth = MediaQuery.of(context).size.width;
     final mediaSize = screenWidth/3;
 
-    return Stack(
-      alignment: AlignmentDirectional.topEnd,
-      children:[
-        SizedBox(
-          width: mediaSize,
-          height: mediaSize,
-          child: widget.media.type == MediaTypes.image
-              ? Image.network(widget.media.value, fit: BoxFit.cover)
-              : VideoPlayer(_videoController!),
-        ),
-        Padding(padding: EdgeInsets.only(right: 8, top: 8),
-        child: widget.media.type == MediaTypes.image
-            ? const Icon(Icons.image, color: Colors.white, size: 16)
-            : const Icon(Icons.video_collection, color: Colors.white, size: 16),
-        ),
-        
-      ]
+    return InkWell(
+      onTap: () => Navigator.push(
+          context, MaterialPageRoute(
+          builder: (context) => UserPostsScreen(
+              userId: widget.userPostMedia.userId,
+              currentScreenIndex: widget.currentScreenIndex, 
+              )
+      )),
+      child: Stack(
+        alignment: AlignmentDirectional.topEnd,
+        children:[
+          SizedBox(
+            width: mediaSize,
+            height: mediaSize,
+            child: widget.userPostMedia.media.type == MediaTypes.image
+                ? Image.network(widget.userPostMedia.media.value, fit: BoxFit.cover)
+                : VideoPlayer(_videoController!),
+          ),
+          Padding(padding: EdgeInsets.only(right: 8, top: 8),
+          child: widget.userPostMedia.media.type == MediaTypes.image
+              ? const Icon(Icons.image, color: Colors.white, size: 16)
+              : const Icon(Icons.video_collection, color: Colors.white, size: 16),
+          ),
+
+        ]
+      ),
     );
   }
 }
