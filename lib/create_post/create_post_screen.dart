1e6dcb3ccc3ee48d1b1e6390cb76057a3d68a581
tumblinger <77502878+tumblinger@users.noreply.github.com>
2025-08-06 20:35:51 -0400

    Update create_post_screen.dart

--- lib/create_post/create_post_screen.dart
+++ lib/create_post/create_post_screen.dart
@@ -1,15 +1,35 @@
 import 'package:flutter/material.dart';
-
+import 'package:instagram_clone/home/media.dart';
 import '../components/app_bottom_navigation_bar.dart';
 
-class CreatePostScreen extends StatelessWidget {
+class CreatePostScreen extends StatefulWidget {
   const CreatePostScreen({super.key});
 
   @override
+  State<CreatePostScreen> createState() => _CreatePostScreenState();
+}
+
+class _CreatePostScreenState extends State<CreatePostScreen> {
+  List<Media> _mediaList = [];
+
+  @override
+  void initState() {
+    if(_mediaList.isEmpty){
+      //open camera
+    }
+    super.initState();
+  }
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(child: Center(child: Text("Create a post Screen"),)),
-        bottomNavigationBar:  AppBottomNavigationBar(currentIndex: 2)
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: const Text('New post', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+      ),
+      body: const SafeArea(
+          child: Center(child: Text("Create a post Screen"),)),
+        bottomNavigationBar:  const AppBottomNavigationBar(currentIndex: 2)
     );
   }
 }
