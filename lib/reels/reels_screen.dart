d6895989dbe2b0082610552b379acee2cd6135d8
tumblinger <77502878+tumblinger@users.noreply.github.com>
2025-08-23 20:17:16 -0400

    Update reels_screen.dart

--- lib/reels/reels_screen.dart
+++ lib/reels/reels_screen.dart
@@ -1,20 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/create_post/posts.dart';
+import 'package:instagram_clone/create_post/posts_service.dart';
+import 'package:instagram_clone/home/media.dart';
+import 'package:instagram_clone/reels/reels_post_list_view.dart';
 
 class ReelsScreen extends StatelessWidget {
-  final List<PostVideo> postVideos;
-  const ReelsScreen({super.key, required this.postVideos});
+
+  ReelsScreen({super.key});
+  final PostsService postsService = PostsService();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: ListView.builder(
-              itemCount: postVideos.length,
-              itemBuilder: (context, index){
-                PostVideo postVideo = postVideos[index];
-               return Container();
-              }
-              )));
+          child: StreamBuilder<List<Posts>>(
+              stream: postsService.getPosts(),
+              builder: (context, snapshot){
+               
+                if(snapshot.connectionState == ConnectionState.waiting){
+                  return const Scaffold(
+                      body: Center(
+                          child: CircularProgressIndicator()));
+                }
+           
+                if(snapshot.hasError){
+                  return const Center(child: Text('Error'));
+                }
+               
+                if(!snapshot.hasData || snapshot.data!.isEmpty){
+                  return const Center(child: Text('No posts yet'));
+                }
+                List<Posts> posts = snapshot.data!;
+                List<PostVideo> postVideos = posts.map((post){
+                  return PostVideo(
+                      posts: post,
+                      videoMedia: post.media.where((mediaItem) => mediaItem.type == MediaTypes.video).toList()
+                  );
+                }).toList();
+
+                return ReelsPostListView(postVideos: postVideos);
+              })
+      ));
   }
 }
