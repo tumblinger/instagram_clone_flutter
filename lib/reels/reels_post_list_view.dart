ef62bd7e0d8eea08b2f40c66564be92287fe7904
tumblinger <77502878+tumblinger@users.noreply.github.com>
2025-08-25 19:22:50 -0400

    Update reels_post_list_view.dart

--- lib/reels/reels_post_list_view.dart
+++ lib/reels/reels_post_list_view.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/create_post/posts.dart';
 import 'package:instagram_clone/reels/reels_media_slider.dart';
+import '../components/app_follow_button.dart';
+import '../components/post_created_by_details.dart';
 
 class ReelsPostListView extends StatelessWidget {
   final List<PostVideo> postVideos;
-  const ReelsPostListView({super.key, required this.postVideos});
+  final int currentScreenIndex;
+  const ReelsPostListView({super.key, required this.postVideos, required this.currentScreenIndex});
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +15,30 @@ class ReelsPostListView extends StatelessWidget {
       itemCount: postVideos.length,
         itemBuilder: (context, index){
       PostVideo postVideo = postVideos[index];
-      return ReelsMediaSlider(videoMediaList: postVideo.videoMedia);
+
+      return Column(
+        children: [
+          ReelsMediaSlider(videoMediaList: postVideo.videoMedia),
+          Container(
+            color: Colors.black12,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 12.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  PostCreatedByDetails(post: postVideo.posts, currentScreenIndex: currentScreenIndex),
+                  Row(children: [
+                    AppFollowButton(),
+                    SizedBox(width: 12.0),
+                    Icon(Icons.more_horiz, color: Colors.white)
+                  ],)
+                ],),
+            ),
+          ),
+        ],
+      );
+
     });
   }
 }
+
