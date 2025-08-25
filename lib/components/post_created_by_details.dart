6eb3406b2dd7612411ed2d75e2c6728d1668f051
tumblinger <77502878+tumblinger@users.noreply.github.com>
2025-08-25 19:07:37 -0400

    Update post_created_by_details.dart

--- lib/components/post_created_by_details.dart
+++ lib/components/post_created_by_details.dart
@@ -1,9 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/create_post/posts.dart';
+
+import '../user_profile/user_page_screen.dart';
+
 class PostCreatedByDetails extends StatelessWidget {
-  const PostCreatedByDetails({super.key});
+  final Posts post;
+  final int currentScreenIndex;
+
+  const PostCreatedByDetails({super.key, required this.post, required this.currentScreenIndex});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return InkWell(
+      onTap: () => Navigator.push(
+          context, MaterialPageRoute(
+          builder: (context) => UserPageScreen(
+              currentScreenIndex: currentScreenIndex,
+              userId: post.userId,
+              userName: post.userName)
+      )),
+      child: Row(
+        children: [
+          SizedBox(
+            width: 28.0,
+            child: CircleAvatar(
+                backgroundImage: NetworkImage(post.avatar)),
+          ),
+
+          SizedBox(width: 12.0),
+
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(post.userName, style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.bold, color: Colors.white)),
+              const Text('Suggested for you', style: TextStyle(fontSize: 10.0, color: Colors.white))
+            ],)
+        ],),
+    );
   }
 }
