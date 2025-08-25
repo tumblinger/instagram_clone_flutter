f168b68746d1ad4fb25b7783742f45859e8eabcd
tumblinger <77502878+tumblinger@users.noreply.github.com>
2025-08-24 20:17:20 -0400

    Create post_created_by_details.dart

---
+++ lib/components/post_created_by_details.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class PostCreatedByDetails extends StatelessWidget {
+  const PostCreatedByDetails({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
