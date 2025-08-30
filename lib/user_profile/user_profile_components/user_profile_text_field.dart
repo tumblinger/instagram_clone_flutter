29a7d4705841841213cea3e39c25554fb1921e48
tumblinger <77502878+tumblinger@users.noreply.github.com>
2025-08-29 20:31:02 -0400

    Update user_profile_text_field.dart

--- lib/user_profile/user_profile_components/user_profile_text_field.dart
+++ lib/user_profile/user_profile_components/user_profile_text_field.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 
 class UserProfileTextField extends StatelessWidget {
-  
+
   final String label;
-  
+
   const UserProfileTextField({super.key, required this.label});
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Text(label),
+        SizedBox(
+          width: 100,
+            child: Text(label)),
         Expanded(child: TextField())
       ],
     );
