d38109eb26edc4849407caa14c076ad938e2fd48
tumblinger <77502878+tumblinger@users.noreply.github.com>
2025-09-12 19:09:06 -0400

    Update user_profile_provider.dart

--- lib/user_profile/user_profile_provider.dart
+++ lib/user_profile/user_profile_provider.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:instagram_clone/auth/auth_service.dart';
@@ -13,14 +15,32 @@ class MyAuthProvider extends ChangeNotifier{
   late SharedPreferences _sharedPreferences;
   String hasSignedUpBeforeStatusKey = 'has-signed-up-before';
   String _email = '';
-  bool _hasSignedUpBefore = false;
-  String ? get email => _email;
 
+  bool _hasSignedUpBefore = false;
+  StreamSubscription<User?>? _authStateSubscription;
+
+  String get email => _email;
   UserProfileModel ? get userProfile => _userProfile;
   bool get hasSignedUpBefore => _hasSignedUpBefore;
 
   MyAuthProvider(){
     _initializeSharedPreferences();
+    _initializeAuthStateListener();
+  }
+
+  void _initializeAuthStateListener(){
+    _authStateSubscription = _authService.firebaseUser.listen((User? firebaseUser) async {
+      if(firebaseUser != null) {
+        try{
+           _userProfile = await _userProfileService.getUserProfile(firebaseUser.uid);
+        } catch(error){
+          print(error);
+          }
+      } else {
+        _userProfile = null;
+        }
+      notifyListeners();
+    });
   }
 
   Future<void> _initializeSharedPreferences() async{
@@ -46,8 +66,6 @@ class MyAuthProvider extends ChangeNotifier{
     notifyListeners();
   }
 
-  //If registration is successful / firebaseUser is NOT null, return the method.
-  //If registration is NOT successful return false:
   Future<AuthServiceResponse<UserProfileModel>> signupWithEmailAndPassword(String password) async {
     final authServiceResponse = await _authService.signupWithEmailAndPassword(
         _email, password);
@@ -56,11 +74,17 @@ class MyAuthProvider extends ChangeNotifier{
 
     if(firebaseUser != null){
       String defaultUserName = _userProfileService.userIDGenerator();
-      String avatarName = firebaseUser.email!.substring(0,3);
-      String randomAvatar = 'https://ui-avatars.com/api/?background=random&name=$avatarName';
+      String avatarName = firebaseUser.email!.substring(0,4);
+      String randomAvatar = 'https://ui-avatars.com/api/?background=0D8ABC&color=001&name=$avatarName';
       User? updatedFirebaseUser = await _authService.updateAuthCurrentUser(defaultUserName, randomAvatar);
+
       setHasSignedUpBefore();
-      _userProfile = UserProfileModel.fromFirebaseUser(firebaseUser);
+      _userProfile = UserProfileModel.fromFirebaseUser(updatedFirebaseUser ?? firebaseUser);
+
+      await _userProfileService.createUserProfile(_userProfile!);
+      _userProfile = await _userProfileService.getUserProfile(firebaseUser.uid);
+      notifyListeners();
+
       return AuthServiceResponse(data: _userProfile);
     }
     return AuthServiceResponse(errorMessage: authServiceResponse.errorMessage);
@@ -69,13 +93,23 @@ class MyAuthProvider extends ChangeNotifier{
   Future<AuthServiceResponse<UserProfileModel>> loginWithEmailAndPassword(String email, String password) async {
     final authServiceResponse = await _authService.loginWithEmailAndPassword(
         email, password);
-
     User? firebaseUser = authServiceResponse.data;
 
     if(firebaseUser != null){
       _userProfile = UserProfileModel.fromFirebaseUser(firebaseUser);
+      notifyListeners();
       return AuthServiceResponse(data: _userProfile);
     }
+
     return AuthServiceResponse(errorMessage: authServiceResponse.errorMessage);
+  }
+  void setUserProfile(UserProfileModel userProfile) {
+    _userProfile = userProfile;
+    notifyListeners();
+  }
+
+  void dispose() {
+    _authStateSubscription?.cancel();
+    super.dispose();
   }
 }
